5016fdd5a5cb44ac4491ccc5ae96bc7decfa0d8f
bensyiek <bensyiek@users.noreply.github.com>
2021-06-16 22:33:16 +0100

    Add cockatrice database code (needs cleanup), work further on scrubbing

--- scriptbase/scripts/mtg/mtgdesign_to_mpc.py
+++ scriptbase/scripts/mtg/mtgdesign_to_mpc.py
@@ -2,10 +2,12 @@ import argparse
 import logging
 import os
 from PIL import Image
+import re
 import tempfile
 
 import scriptbase.file_handling.file_utils as file_utils
 import scriptbase.file_handling.image_utils as image_utils
+import scriptbase.magic_the_gathering.cockatrice as cockatrice
 
 
 this_logger = logging.getLogger(__loader__.name)
@@ -14,8 +16,14 @@ this_logger = logging.getLogger(__loader__.name)
 ARTIST_X_RANGE = range(140, 320)
 ARTIST_Y_RANGE = range(990, 1013)
 
-COPYRIGHT_X_RANGE_CREATURE = range(440, 698)
+COPYRIGHT_X_RANGE = range(440, 698)
 COPYRIGHT_Y_RANGE_CREATURE = range(986, 1000)
+COPYRIGHT_Y_RANGE_NONCREATURE = range(969, 984)
+
+CORNER_RANGES = [(range(0, 35), range(0, 34)),
+                 (range(708, 744), range(0, 34)),
+                 (range(0, 35), range(1006, 1039)),
+                 (range(708, 744), range(1006, 1039))]
 
 
 def parse_args():
@@ -27,6 +35,11 @@ def parse_args():
     parser.add_argument("-i", "--input", help="Location of folder containing card images", required=True)
     parser.add_argument("-v", "--valid-extensions", default=["jpg", "png"], help="List of valid image extensions",
                         nargs='+')
+    parser.add_argument("-c", "--cockatrice-xml", help="More accurately erase copyright by passing a cockatrice XML "
+                                                       "of the set")
+    parser.add_argument("-r", "--regex-name-match", help="Fancy name matching to extract a card name from a file name",
+                        default="(.+)")
+    ## [\d]+_(.+) - what the dndmtg set uses, second capture group
 
     ## Flags
     parser.add_argument("-A", "--scrub-artist", help="Erases the artist from the card image", action="store_true")
@@ -59,6 +72,12 @@ def main():
     if not os.path.exists(args.output):
         os.makedirs(args.output)
 
+    cockatrice_database = None
+    if args.cockatrice_xml:
+        this_logger.warning(f"LOADING COCKATRICE DATABASE...")
+        cockatrice_database = cockatrice.CockatriceDatabase()
+        cockatrice_database.parse_xml(path=args.cockatrice_xml)
+
     valid_files = file_utils.recursive_file_grab(args.valid_extensions, args.input)
     valid_files.sort()
 
@@ -77,13 +96,26 @@ def main():
         this_logger.warning(f"({i}/{total}) Processing file {image_path}...")
 
         try:
+            im_filename = os.path.splitext(os.path.basename(image_path))[0]
+            card_name = re.match(args.regex_name_match, im_filename).group(1)
             im: Image.Image = Image.open(image_path)
+
+            card_is_creature = True
+            if cockatrice_database is not None:
+                card_is_creature = cockatrice_database.card_data.get(card_name, "Non-creature") == "Creature"
 
             if args.scrub_artist:
                 image_utils.scrub(im, ((x, y) for x in ARTIST_X_RANGE for y in ARTIST_Y_RANGE))
 
             if args.scrub_copyright:
-                image_utils.scrub(im, ((x, y) for x in COPYRIGHT_X_RANGE for y in COPYRIGHT_Y_RANGE))
+                if card_is_creature:
+                    image_utils.scrub(im, ((x, y) for x in COPYRIGHT_X_RANGE for y in COPYRIGHT_Y_RANGE_CREATURE))
+                else:
+                    image_utils.scrub(im, ((x, y) for x in COPYRIGHT_X_RANGE for y in COPYRIGHT_Y_RANGE_NONCREATURE))
+
+            if args.scrub_corners:
+                for x_range, y_range in CORNER_RANGES:
+                    image_utils.scrub(im, ((x, y) for x in x_range for y in y_range))
 
             im.save(os.path.join(args.output, os.path.basename(image_path)))
 
