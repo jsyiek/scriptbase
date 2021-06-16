377aaff52a57a403544620726f4eec4071accdb6
bensyiek <bensyiek@users.noreply.github.com>
2021-06-16 20:52:16 +0100

    Iniital commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='scriptbase',
+    version='0.1.0',
+    description="Jamie's scripts",
+    author="Jamie Syiek",
+    author_email="jamiesyiek@gmail.com",
+    packages=['scriptbase']
+)
