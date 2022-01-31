6a2250bc926a92e419348f1ef1339243fdc08f52
Jamie Syiek <jamiesyiek@gmail.com>
2022-01-31 11:06:16 +0000

    modifications to doc strings

--- scriptbase/scripts/cybersecurity/deceptive_domain_score.py
+++ scriptbase/scripts/cybersecurity/deceptive_domain_score.py
@@ -21,7 +21,9 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Computes the deceptive domain score for a given domain.")
 
     parser.add_argument("-d", "--domain", help="Domain name to compute score for", required=True)
-    parser.add_argument("-c", "--company-data", help="Company data to compare against",
+    parser.add_argument("-c", "--company-data", help="Path to a CSV file containing company data to compare against. "
+                                                     "Two columns required (no headers): second-level domain and "
+                                                     "top-level domain",
                         default=os.path.join(SCRIPTBASE_DIRECTORY, "examples/demo_files/company_data.csv"))
     parser.add_argument("-q", "--quiet", help="Significantly reduces logging output; "
                                               "just prints the score", action="store_true")
@@ -47,8 +49,19 @@ def clean_input(website_url: str) -> Tuple[str, list]:
                           changes are made
     """
 
+    # Removes http(s)://www. from the start of the website
     cleaned_domain = re.sub(HTTP_HTTPS_PATTERN, "", website_url)
+
+    # Non-alphanumeric characters are rare in domain names but add unnecessary noise to the score.
+    # we remove any that are not strictly numbers or digits
+    # E.g. removing '-' from face-book.com
+    # Removes all non-alphanumeric characters.
     cleaned_domain = re.sub(REPEATED_NONALPHANUMERIC_CHARACTERS, "", cleaned_domain)
+
+    # the 0s in face0000000book.com add unnecessary noise to the score, so we remove the extraneous 0s
+    # to get a better understanding of what the domain really is.
+    # Removes any 3 consecutive numbers from the domain name. It's rare that there are three numbers in a
+    # legitimate domain name (at least from my memory)
     cleaned_domain = re.sub(UNNECESSARY_NUMBERS_PATTERN, "", cleaned_domain)
 
     if website_url != cleaned_domain:
@@ -62,7 +75,7 @@ def tokenize(string_to_tokenize: str, length: int) -> Iterator[str]:
     of string_to_tokenize
 
     Parameters:
-         string_to_tokenize (str): String to generate contingous tokens of size length from
+         string_to_tokenize (str): String to generate contiguous tokens of size length from
          length (int): Size of token to generate
 
     Yields:
@@ -128,9 +141,17 @@ def phish_target_score(domain_to_check: Tuple[str, str],
 
     Scoring mechanism:
     1) Begin with a score of 10
-    2) Weight by the edit distance according to parameters
+    2) Weight by the minimum edit distance from a given site.
+        a) `edit_distance_weightings parameters correspond to specific edit distances and multiply the score by
+           that amount.
     3) Weight by the proportion that the company name occupies in the domain name
+        a) `subsection_size_weighting` is the amount that the proportion is raised to.
+        b) This proportion is assumed to be <= 1, so we take the minimum of 1/company_name_proportion and
+           company_name_proportion where company_name_proportion is len(company_name)/len(domain)
     4) Weight by whether or not the domain to check is a .com domain
+        a) A domain may be less suspicious if it is a .com domain
+    5) Weight by whether or not the domain to check has the same top-level domain as the potential target
+        a) If impersonating a domain, it adds false legitimacy to have the same top-level domain
 
     Parameters:
         domain_to_check (Tuple[str, str]): Domain to compute phish target score for
